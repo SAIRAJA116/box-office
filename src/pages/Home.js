87b118e153b612bc5e420410a69587cad94e6d5b
SAIRAJA116 <sairaja899@gmail.com>
2022-03-21 12:41:08 +0530

    added layout management and created title

---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+/* eslint-disable  */
+import React from 'react'
+import MainPageLayout from '../components/MainPageLayout'
+
+const Home = () => {
+  return (
+    <MainPageLayout>This is  home </MainPageLayout>
+  )
+}
+
+export default Home
---
+++ src/pages/Starred.js
@@ -0,0 +1,10 @@
+/* eslint-disable */
+import React from 'react'
+
+const Starred = () => {
+  return (
+    <div>This is Starred</div>
+  )
+}
+
+export default Starred
