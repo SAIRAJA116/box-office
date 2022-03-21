837dd64c468f04f26382299320a518bd14c20bd6
SAIRAJA116 <sairaja899@gmail.com>
2022-03-21 11:23:18 +0530

    added react router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-
+import {Switch,Route} from 'react-router-dom'
 
 function App() {
   return (
-    <div >
-      hi
-    </div>
+    <Switch>
+      <Route exact path="/">This is home page</Route>
+      <Route exact path="/starred">This is starred</Route>
+      <Route>Undefined route This page is not found 404</Route>
+    </Switch>
   );
 }
 
